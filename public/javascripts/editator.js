c410cf1324599727aaa67bb9df3a50b16f9b38c7
Ben Hyland <mail@bhyland.co.uk>
2013-05-01 23:37:59 +0000

    add a form for user name

--- public/javascripts/editator.js
+++ public/javascripts/editator.js
@@ -2,4 +2,13 @@ function Editator($scope) {
 	$scope.room = {
 		'users': [ {'name': 'fred'}, {'name': 'bob'} ]
 	}
+	
+	$scope.user = {
+		'nick': ''
+	}
+
+	$scope.updateNick = function() {
+		// todo: send to server and subscribe to room change events - atmosphere?
+		this.room.users.push({'name': this.user.nick})
+	}
 }
